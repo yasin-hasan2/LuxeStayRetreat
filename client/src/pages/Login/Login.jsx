24585141cb17bb83dba48f46838d3021dee58471
yasin al hasan <ya705668@gmail.com>
2024-10-12 16:58:14 +0600

    add login and logout

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,85 +1,134 @@
-import { Link } from 'react-router-dom'
-import { FcGoogle } from 'react-icons/fc'
+import { Link, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import useAuth from "../../hooks/useAuth";
+// import axios from "axios";
+import toast from "react-hot-toast";
+import { ImSpinner9 } from "react-icons/im";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { signInWithGoogle, loading, setLoading, signIn } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const from = e.target;
+    const email = from.email.value;
+    const password = from.password.value;
+
+    try {
+      setLoading(true);
+
+      //2.User  SignIn
+      await signIn(email, password);
+      navigate("/");
+      toast.success("signUp successful");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
+
+  // handle google signing
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+
+      navigate("/");
+      toast.success("SignUp Successful");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
-        <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Log In</h1>
-          <p className='text-sm text-gray-400'>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Log In</h1>
+          <p className="text-sm text-gray-400">
             Sign in to access your account
           </p>
         </div>
         <form
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
+          onSubmit={handleSubmit}
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
-          <div className='space-y-4'>
+          <div className="space-y-4">
             <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
+              <label htmlFor="email" className="block mb-2 text-sm">
                 Email address
               </label>
               <input
-                type='email'
-                name='email'
-                id='email'
+                type="email"
+                name="email"
+                id="email"
                 required
-                placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
               />
             </div>
             <div>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm mb-2'>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
                   Password
                 </label>
               </div>
               <input
-                type='password'
-                name='password'
-                autoComplete='current-password'
-                id='password'
+                type="password"
+                name="password"
+                autoComplete="current-password"
+                id="password"
                 required
-                placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
+                placeholder="*******"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
               />
             </div>
           </div>
 
           <div>
             <button
-              type='submit'
-              className='bg-rose-500 w-full rounded-md py-3 text-white'
+              disabled={loading}
+              type="submit"
+              className="bg-rose-500 w-full rounded-md py-3 text-white"
             >
-              Continue
+              {loading ? (
+                <ImSpinner9 className="animate-spin text-2xl m-auto text-yellow-300" />
+              ) : (
+                "Continue"
+              )}
             </button>
           </div>
         </form>
-        <div className='space-y-1'>
-          <button className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
+        <div className="space-y-1">
+          <button className="text-xs hover:underline hover:text-rose-500 text-gray-400">
             Forgot password?
           </button>
         </div>
-        <div className='flex items-center pt-4 space-x-1'>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-          <p className='px-3 text-sm dark:text-gray-400'>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+          <p className="px-3 text-sm dark:text-gray-400">
             Login with social accounts
           </p>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignIn}
+          className=" disabled:cursor-not-allowed cursor-pointer flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded"
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
-        <p className='px-6 text-sm text-center text-gray-400'>
-          Don&apos;t have an account yet?{' '}
+        </button>
+        <p className="px-6 text-sm text-center text-gray-400">
+          Don&apos;t have an account yet?{" "}
           <Link
-            to='/signup'
-            className='hover:underline hover:text-rose-500 text-gray-600'
+            to="/signup"
+            className="hover:underline hover:text-rose-500 text-gray-600"
           >
             Sign up
           </Link>
@@ -87,7 +136,7 @@ const Login = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
