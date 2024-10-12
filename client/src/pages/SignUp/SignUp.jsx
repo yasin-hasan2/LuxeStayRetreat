c881d24d3be5c2c4f31d5d8c911513433c0d0447
yasin al hasan <ya705668@gmail.com>
2024-10-12 12:34:46 +0600

    add signUp system with functional

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,103 +1,176 @@
-import { Link } from 'react-router-dom'
-import { FcGoogle } from 'react-icons/fc'
+import { Link, useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { ImSpinner9 } from "react-icons/im";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const {
+    createUser,
+    signInWithGoogle,
+    loading,
+    setLoading,
+    updateUserProfile,
+  } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const from = e.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const password = from.password.value;
+    const image = from.image.files[0];
+
+    const formData = new FormData();
+    formData.append("image", image);
+
+    try {
+      setLoading(true);
+      // 1. upload image and get image uri
+      const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMGBB_API_KEY
+        }`,
+        formData
+      );
+      const photo = data.data.display_url;
+      console.log(data, photo);
+
+      //2.User  registration
+      const result = await createUser(email, password);
+      console.log(result);
+
+      //3. Save username and photo is firebase
+      await updateUserProfile(name, photo);
+      navigate("/");
+      toast.success("signUp successful");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
+
+  // handle google signing
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+
+      navigate("/");
+      toast.success("SignUp Successful");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
-        <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
-          <p className='text-sm text-gray-400'>Welcome to StayVista</p>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
+          <p className="text-sm text-gray-400">Welcome to StayVista</p>
         </div>
         <form
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
+          onSubmit={handleSubmit}
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
-          <div className='space-y-4'>
+          <div className="space-y-4">
             <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
+              <label htmlFor="email" className="block mb-2 text-sm">
                 Name
               </label>
               <input
-                type='text'
-                name='name'
-                id='name'
-                placeholder='Enter Your Name Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter Your Name Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
               />
             </div>
             <div>
-              <label htmlFor='image' className='block mb-2 text-sm'>
+              <label htmlFor="image" className="block mb-2 text-sm">
                 Select Image:
               </label>
               <input
                 required
-                type='file'
-                id='image'
-                name='image'
-                accept='image/*'
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
               />
             </div>
             <div>
-              <label htmlFor='email' className='block mb-2 text-sm'>
+              <label htmlFor="email" className="block mb-2 text-sm">
                 Email address
               </label>
               <input
-                type='email'
-                name='email'
-                id='email'
+                type="email"
+                name="email"
+                id="email"
                 required
-                placeholder='Enter Your Email Here'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
-                data-temp-mail-org='0'
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
               />
             </div>
             <div>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm mb-2'>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
                   Password
                 </label>
               </div>
               <input
-                type='password'
-                name='password'
-                autoComplete='new-password'
-                id='password'
+                type="password"
+                name="password"
+                autoComplete="new-password"
+                id="password"
                 required
-                placeholder='*******'
-                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900'
+                placeholder="*******"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
               />
             </div>
           </div>
 
           <div>
             <button
-              type='submit'
-              className='bg-rose-500 w-full rounded-md py-3 text-white'
+              disabled={loading}
+              type="submit"
+              className="bg-rose-500 w-full rounded-md py-3 text-white"
             >
-              Continue
+              {loading ? (
+                <ImSpinner9 className="animate-spin text-2xl m-auto text-yellow-300" />
+              ) : (
+                "Continue"
+              )}
             </button>
           </div>
         </form>
-        <div className='flex items-center pt-4 space-x-1'>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-          <p className='px-3 text-sm dark:text-gray-400'>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+          <p className="px-3 text-sm dark:text-gray-400">
             Signup with social accounts
           </p>
-          <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignIn}
+          className=" disabled:cursor-not-allowed cursor-pointer flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded"
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
-        <p className='px-6 text-sm text-center text-gray-400'>
-          Already have an account?{' '}
+        </button>
+        <p className="px-6 text-sm text-center text-gray-400">
+          Already have an account?{" "}
           <Link
-            to='/login'
-            className='hover:underline hover:text-rose-500 text-gray-600'
+            to="/login"
+            className="hover:underline hover:text-rose-500 text-gray-600"
           >
             Login
           </Link>
@@ -105,7 +178,7 @@ const SignUp = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
